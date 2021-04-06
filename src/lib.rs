60ba0ba514d63e3d2c88c548530f986a880e1d1a
Santtu Söderholm <santtu.soderholm@protonmail.com>
2021-04-06 14:52:58 +0300

    Changed documentation in README and crate root

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
 /*!
-This Rust library defines a mapping between SISU attainments and Europass credentials.
-SISU attainments retrievable via GET requests are described in
-the [SISU Swagger UI](https://sis-tuni.funidata.fi/ori/swagger-ui.html#/attainment-controller/getAttainmentsUsingGET),
-whereas information concerning the Europass credentials can be found on Github,
-under the [Europass Learning Model documentation](https://github.com/european-commission-europass/Europass-Learning-Model/tree/master/Credentials).
+# ↦ Europass Credential
 
-SISU provides the attainments in JSON format,
-whereas the Europass API accepts XML documents as input.
-The mapping therefore needs to perform this JSON → XML transformation.
-The fields in the JSON provided by SISU also do not match the ones in Europass XML documents,
-so in addition to a format transformation, a field transformation is performed as well.
+This Rust library defines a mapping between university-specific accreditations
+and Europass credentials. The Europass API accepts
+[XML documents](https://github.com/european-commission-europass/Europass-Learning-Model/tree/master/Credentials) as input.
+The mapping therefore needs to perform an `<ATTAINMENT FORMAT>` → XML transformation.
+The fields of university-specific attainments are also
+unlikely to have a one-to-one mapping between the
+[Europass Credential XML Model](https://github.com/european-commission-europass/Europass-Learning-Model/blob/master/Credentials/Credentials_Learning_Model.md)
+fields, so this transformation needs to be performed as well.
+
+## Implemented conversions (or conversions being worked on)
+
+This section lists the conversions that have been implemented
+and some details related to each conversion.
+
+### Tampere University
+
+SISU attainments retrievable via GET requests are described in
+the [SISU Swagger UI](https://sis-tuni.funidata.fi/ori/swagger-ui.html#/attainment-controller/getAttainmentsUsingGET).
+SISU provides the attainments in JSON format.
 */
 
 mod sisu_attainment;
